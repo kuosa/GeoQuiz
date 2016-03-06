67adb8235c225f23fb48ef5a3048e752c4a68131
kuosa <su.a.kuo@gmail.com>
2016-03-05 19:26:08 -0800

    First commit of project

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -0,0 +1,36 @@
+package com.bignerdranch.android.geoquiz;
+
+/**
+ * Created by suchi on 12/5/15.
+ */
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+
+
+    public Question(int textResId, boolean answerTrue)
+
+    {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+}
